5a9ef597519dfd035a1e97907eaa3640f4815ec1
sumit jolly <sumit_jolly@mckinsey.com>
2017-06-04 17:46:51 +0530

    updated readme

--- api/deploy/deploy.sh
+++ api/deploy/deploy.sh
@@ -2,4 +2,5 @@ ssh sumitjolly@myweatherforecast.southindia.cloudapp.azure.com
 cd WeatherForecast/
 git pull
 sudo pkill -f node
-sudo npm start > stdout.txt 2> stderr.txt &
+sudo NODE_ENV=production npm start > stdout.txt 2> stderr.txt &
+sudo serve -s -p build > stdout.txt 2> stderr.txt &
